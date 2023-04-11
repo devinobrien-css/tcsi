f57248f401b8e104c0538b5c2baa753b2441f2f3
Robert Hamilton <116108705+riobairdnahibernian@users.noreply.github.com>
2023-04-11 09:20:02 -0400

    Update Depthfullness.page.jsx

--- src/pages/Depthfullness.page.jsx
+++ src/pages/Depthfullness.page.jsx
@@ -19,41 +19,33 @@ const Depthfullness = () => {
                 Depthfullness
             </SectionHeader>
             <TextSection  >
-                Depthfulness
-<P> As should be clear from all of the above - Sympathetic Intelligence is an indelible part of who we are and
-how we behave. We all have the capacity for Sympathetic Intelligence and Its presence is ubiquitous.
-However, what should also be clear from our enumeration of the various parameters above - exercising
-Sympathetic Intelligence is a challenge.
-Depthfulness is a means of developing the capacity to overcoming that challenge and apply our
-Sympathetic Intelligence to greater effect, and with greater benefit to all whom we come in contact with.
-Depthfulness, is the acquisition and application of an optimized Sympathetic Intelligence. Depthfulness is
-two things - First, it is a concept, and Second it is an acquired ability. The purpose behind training in
-Sympathetic Intelligence is to acquire the latter through understanding the former.
-The concept is found in the structure of Sympathetic Intelligence and the manner in which our individual
-mechanisms interface with that framework. At the Center for Sympathetic Intelligence, we believe that
-to the extent any individual can contemplate, become aware of and embrace the concept, then to that
-extent they will acquire depthfulness and be able to better engage their own capacity for Sympathetic
-Intelligence. So, concept plus training brings an awareness, which leads to acquired ability and in the
-result, they will be better connected to each moment and better able to influence each moment both
-positively and constructively.</P>
-        
-So, depthlessness is a way of taking control of our capacity for Sympathetic Intelligence - it is a learned,
-disciplined skill unto itself. It brings awareness of the self in the moment BUT, from the perspective of
-the context. While mindfulness means focussing on the self in context, depthfulness means focusing on
-the context with the self in it. Mindfulness is a passive acceptance of the current circumstances, while
-depthfulness is an active and willful engagement with the context. Police officers who develop it, will no
-longer just be thinking from the inside out - they will be thinking from the outside in. Depthfulness is
-deployed both contemplatively in training and preemptively in day to day operations. Depthfulness helps
-us more appropriately align our Sympathetic Intelligence with the changing circumstances as they unfold from moment to moment, allowing us to connect with each moment in accordance with the specifics of
-each new moment, and the specific context.
-        
-Training for improved Sympathetic Intelligence means studying, contemplating and practicing the
-characteristics of Sympathetic intelligence - understanding the Tiers, the relationship between them and
-the application and operation of all of the parameters outlined above. We believe that the awareness gained and the ability to act on that awareness will empower those who receive the training to use their
-sympathetic intelligence to greater effect in delivering on their promise to serve and protect their
-community. From the diagram above, we can see that the majority of components are present in each
-tier and that Sympathetic Intelligence through Depthfulness is the pathway to an operational form of
-Collective Intelligence.
+                Application of Sympathetic Intelligence
+Sympathetic Intelligence is an indelible part of who we are and how we behave. We all have the capacity for Sympathetic Intelligence and its presence is ubiquitous. However, perhaps because Sympathetic Intelligence is so much a part of who we are and how we are, exercising Sympathetic Intelligence consciously or deliberately presents as a challenge.
+ 
+Depthfulness is an approach that we are developing here at the Center to overcome that challenge. The goal is to create a means and capacity so that individuals may be able to bring their natural Sympathetic Intelligence into their daily lives in a manner that will have greater effect, and bring greater benefit to all whom they come into contact with. We believe that if we can deliver this refined human ability to bear in professional environments we can bring an immediate benefit to all of our lives. This is why at the outset we have created three core panels:- Education, Healthcare and Policing.
+ 
+What is Depthulness
+Depthfulness, is the acquisition and application of an optimized Sympathetic Intelligence. Depthfulness is two things - First, it is a concept, and Second it is an acquired ability. 
+ 
+The purpose behind training in Sympathetic Intelligence is to acquire Depthfulness through understanding Sympathetic Intelligence. That is because the concept of Depthfulness is found in the structure of Sympathetic Intelligence and the manner in which our individual mechanisms interface with that framework. 
+ 
+At the Center for Sympathetic Intelligence, we believe that to the extent any individual can contemplate, become aware of and embrace the concept of Sympathetic Intelligence, then to that extent they will acquire Depthfulness and be able to better engage their own capacity for Sympathetic Intelligence. 
+ 
+What is Depthulness Training
+Stated simply, training brings an awareness, which leads to an acquired ability and in the result, each individual will be better connected to each moment and better able to influence each moment both positively and constructively.
+ 
+So, Depthfulness Training is a way to facilitate the individual taking control of their individual capacity for Sympathetic Intelligence - it is a learned, disciplined skill unto itself. It brings awareness of the self in the moment BUT, from the perspective of the context. 
+ 
+We find that a useful way to understand Depthfulness is to compare or relate it to Mindfulness. For the purposes of this comparison, Mindfulness means focusing on the self in context, while Depthfulness means focusing on the context with the self in it. Mindfulness is a passive acceptance of the current circumstances, while Depthfulness is an active and willful engagement with the context. Professionals and others who develop it, will no longer just be thinking from the inside out - they will also be thinking from the outside in. 
+ 
+Depthfulness is deployed both contemplatively in training and preemptively in day to day operations. Depthfulness helps us more appropriately align our Sympathetic Intelligence with the changing circumstances as they unfold from moment to moment, allowing us to connect with each moment in accordance with the specifics of each new moment, and the specific context.        
+ 
+Training for improved Sympathetic Intelligence means studying, contemplating and practicing the characteristics of Sympathetic intelligence - understanding the Tiers, the relationship between them and the application and operation of all of the parameters outlined above. 
+ 
+We believe that the awareness gained and the ability to act on that awareness will empower those who receive the training to use their sympathetic intelligence to greater effect in delivering on their promise to serve and protect their community. 
+ 
+From the point of view of a Community or an Institution - Depthfulness is the pathway to an operational form of Collective Intelligence.
+
             </TextSection>
         </Section>
     )
