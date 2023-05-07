55a70a267cd8c3e894230aadc1253c1123a6ce24
Robert Hamilton <116108705+riobairdnahibernian@users.noreply.github.com>
2023-05-07 15:09:53 -0400

    Update Applications.page.jsx

--- src/pages/Applications.page.jsx
+++ src/pages/Applications.page.jsx
@@ -34,13 +34,14 @@ const Applications = () => {
                 Applications
             </SectionHeader>
             <TextSection>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Iure dolorem nemo cum consequuntur, incidunt vero odio 
-                harum voluptates qui, totam repellat! 
+                As we learn more about the phenomenon of sympathetic intelligence, we realize that the possibilities for its application are simply endless. There is simply no part of the human existence that does not involve sympathetic intelligence to at least some degree, however small.
+
+Also, at the center for sympathetic intelligence we have realized that the introduction of the concept will take time to develop and we believe it's application is more about optimizing and understanding the presence that it already has in our lives. In other words the more informed we are about it the more likely we are able to apply it in a constructive and more contributive manner.
+
+One aspect about the concept that becomes clear is that there is a macro to micro characteristic. So, in order to both take advantage of that fact and begin the process of developing and applying the concept we are creating panels that address large professional communities within society. We believe this is the best way to begin. 3 of the panels that we have began are; The Education panel, The Health care panel, and The Police Panel.
+
+We will add more panels as the work of the center develops. 
                 <Hyperlink to="#">This is a hyperlink</Hyperlink> 
-                Obcaecati iusto, beatae ab esse ipsam perferendis, modi unde architecto 
-                fugit quo ipsum temporibus atque, nesciunt saepe officiis in 
-                eum sint deleniti.
             </TextSection>
             <Note id="note">
                 Here's an example of a note
